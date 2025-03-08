b2ff03bdd6c95b58b74691a1cdd27b2bd2e9d617
Raktim Raihan Prova <raktim.raihan.prova@gmail.com>
2025-03-09 01:48:56 +0900

    Add Redux Toolkit for state management
    Initialized Redux Toolkit in the project with a user slice and store setup. Updated `index.tsx` to include the Redux Provider and configured state types for better TypeScript support. Added required dependencies and configurations in `package.json` and `package-lock.json`.

---
+++ todo-frontend/src/slices/userSlice.ts
@@ -0,0 +1,52 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+interface User {
+    username: string;
+    emailAddress: string;
+    updatedAt: Date;
+    createdAt: Date;
+    loggedIn: boolean;
+    accessToken: string;
+}
+
+const initialState: User = {
+    username: '',
+    emailAddress: '',
+    updatedAt: new Date(),
+    createdAt: new Date(),
+    loggedIn: false,
+    accessToken: ''
+}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        login: (state,
+                action: PayloadAction<{
+                    username: string,
+                    email: string,
+                    updatedDate: Date,
+                    createdDate: Date,
+                    accessToken: string}>) => {
+            state.username = action.payload.username;
+            state.emailAddress = action.payload.email;
+            state.updatedAt = action.payload.updatedDate;
+            state.createdAt = action.payload.createdDate;
+            state.loggedIn = true;
+            state.accessToken = action.payload.accessToken;
+        },
+
+        logout: (state) => {
+            state.username = '';
+            state.emailAddress = '';
+            state.updatedAt = new Date();
+            state.createdAt = new Date();
+            state.loggedIn = false;
+            state.accessToken = '';
+        }
+    }
+});
+
+export const {login, logout} = userSlice.actions;
+export const userReducer =  userSlice.reducer;
